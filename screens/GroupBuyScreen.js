095f2f90cbe73289f8af5f762ff3448c8dfd55ce
Kayton <kt.kaytonheng@gmail.com>
2021-06-14 19:54:33 +0800

    Added firebase into Leon's branch

--- screens/GroupBuyScreen.js
+++ screens/GroupBuyScreen.js
@@ -1,17 +1,82 @@
-import * as React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  Image,
+  Button,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import firebase from "../database/firebaseDB";
+
+const db = firebase.firestore().collection("shopInfo");
 
 export default function GroupBuyScreen() {
+
+  const [shopInfo, setShopInfo] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = firebase 
+      .firestore() 
+      .collection("shopInfo")
+      .onSnapshot((collection) => {
+        const updatedShopInfo = collection.docs.map((doc) => doc.data());
+        setShopInfo(updatedShopInfo);
+      })
+
+      return () => {unsubscribe();}
+  }, [])
+
+  // The function to render each row in our FlatList
+  function renderItem({ item }) {
+    return (
+      <View
+        style={{
+          padding: 10,
+          paddingTop: 20,
+          paddingBottom: 20,
+          borderBottomColor: "#ccc",
+          borderBottomWidth: 1,
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text>{item.shopName}</Text>
+        <Text>{item.shopLocation}</Text>
+        <Image 
+          source={{uri:item.shopMenu}}
+          style={styles.listImage}
+        />
+        <TouchableOpacity onPress={() => deleteNote(item.id)}>
+          <Ionicons name="trash" size={16} color="#944" />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "lightpink",
-      }}
-    >
-      <Text>Contacts!</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={shopInfo}
+        renderItem={renderItem}
+        style={{ width: "100%" }}
+        keyExtractor={(item) => item.id.toString()}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffc",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  listImage: {
+    width: 50,
+    height: 50,
+  }
+});
