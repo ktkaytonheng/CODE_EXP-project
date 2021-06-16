b8ec26a3e3a3b4676ccb877472a546dc0687cdfe
Zufiqqar <57473144+Zufiqqar@users.noreply.github.com>
2021-06-16 17:56:59 +0800

    Update AddListScreen.js

--- screens/AddListScreen.js
+++ screens/AddListScreen.js
@@ -1,12 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { Text, View, StyleSheet, Image, TextInput,TouchableOpacity } from "react-native";
+import { Text, View, StyleSheet, Image, TextInput,TouchableOpacity, Button } from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 import firebase from "../database/firebaseDB";
+import {Picker} from '@react-native-picker/picker';
 
 
 export default function AddListScreen({ route }) {
-  const { id, shopName, shopMenu, shopLocation} = route.params;
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const { shopName, shopMenu, shopLocation} = route.params;
   const [paxes, setPax] = useState("");
-  const [timing, setTime] = useState(""); 
+  const [timing, setTime] = useState(new Date().toLocaleString('en-GB', { timeZone: 'UTC' }));
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date) => {
+    setTime(date.toLocaleString('en-GB', { timeZone: 'UTC' }));
+    hideDatePicker();
+  };
 
   function increment() {
     if (!timing.trim()) {
@@ -17,10 +33,10 @@ export default function AddListScreen({ route }) {
       alert('Please Enter Pax');
       return;
     }
-    firebase.firestore().collection("test").add({
+    firebase.firestore().collection("Orders").add({
       pax: paxes,
       picker: "currentUser",
-      shopID: id,
+      shopID: shopName,
       time: timing,
     });
     alert('Success');
@@ -31,30 +47,40 @@ export default function AddListScreen({ route }) {
       <Text style={{margin:3, textAlign: 'center'}}>{shopName}</Text>
       <View style={{alignItems:"center", justifyContent: 'center'}}>
         <Image style={styles.image} source={{uri: shopMenu}}/>
-        <Text style={{fontSize: 20}}>Add List</Text>
-        <TextInput 
-          style={styles.textArea} 
-          placeholder={'Pax'}
-          multiline={true}
-          onChangeText={
-            (value) => setPax(value)
-          }/>
-        <TextInput 
-          style={styles.textArea} 
-          placeholder={'Time'} 
-          multiline={true}
-          onChangeText={
-            (value) => setTime(value)
-          }/>
-        <TextInput 
-          style={styles.textArea}  
-          value={shopLocation}
-          numberOfLines={4}
-          multiline={true}/>
-        <TouchableOpacity style={styles.submit} onPress={increment}>
-          <Text style={styles.button}>Submit</Text>
-        </TouchableOpacity>
+        {/*<Text style={{fontSize: 20}}>Add List</Text>*/}
       </View>
+        <View style={{alignItems:"flex-start", justifyContent: 'flex-start', marginStart:"20%"}}>
+          <Text style={styles.text}>Stall Location</Text>
+          <Text style={styles.textArea}>{shopLocation}</Text>
+          <Text style={styles.text}>Enter time</Text>
+          <Button style={styles.textArea} title={timing} onPress={showDatePicker} />
+          <DateTimePickerModal
+            isVisible={isDatePickerVisible}
+            mode="time"
+            onConfirm={handleConfirm}
+            onCancel={hideDatePicker}
+          />
+          <Text style={styles.text}>Enter pax</Text>
+          <Picker
+            style={styles.pickerStyle} 
+            selectedValue={paxes}
+            style={{ height: 50, width: 150 }}
+            onValueChange={(itemValue, itemIndex) => setPax(itemValue)}
+          >
+            <Picker.Item label="1" value="1" />
+            <Picker.Item label="2" value="2" />
+            <Picker.Item label="3" value="3" />
+            <Picker.Item label="4" value="4" />
+            <Picker.Item label="5" value="5" />
+            <Picker.Item label="6" value="6" />
+            <Picker.Item label="7" value="7" />
+            <Picker.Item label="8" value="8" />
+            <Picker.Item label="9" value="9" />
+          </Picker>
+          <TouchableOpacity style={styles.submit} onPress={increment}>
+            <Text style={styles.button}>Submit</Text>
+          </TouchableOpacity>
+        </View>
     </View>
   );
 }
@@ -76,15 +102,17 @@ const styles = StyleSheet.create({
   textArea: {
     justifyContent: "center",
     alignItems:"center",
-    width: "70%",
-    padding: 5,
+    width: "75%",
+    padding: 15,
     margin: 2,
-    borderRadius: 10,
     borderColor: "grey",
     borderWidth: 1
   },
+  text: {
+    marginTop: 5,
+  },
   submit: {
-    width: "70%",
+    width: "75%",
     padding: 4,
     margin: 10,
     color: "white",
@@ -96,5 +124,13 @@ const styles = StyleSheet.create({
   button: {
     textAlign: "center",
     color: "white",
-  }
+  },
+  pickerStyle:{  
+    width: "80%",  
+    color: 'red',  
+    justifyContent: 'center',
+    color: "red",  
+    borderWidth: 20,
+    borderColor: "black"
+  },
 });
